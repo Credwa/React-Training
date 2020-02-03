ae0936e6d25974e506e2e6399c583195503021d3
Craig Edwards <craigroe7@gmail.com>
2020-02-03 11:25:04 -0500

    prod build

--- src/index.js
+++ src/index.js
@@ -12,12 +12,15 @@ import burgerBuilderReducer from 'store/reducers/burgerBuilder.reducer';
 import orderReducer from 'store/reducers/order.reducer';
 import authReducer from 'store/reducers/auth.reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
   auth: authReducer
 });
+
+console.log(process.env.NODE_ENV);
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
