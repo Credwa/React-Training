4dc42e628f7d6e0307f38ac8d16c08174114da73
Craig Edwards <craigroe7@gmail.com>
2020-02-03 11:25:04 -0500

    prod build

---
+++ precache-manifest.07ea07a333290eef366847aa86433df4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4205a652bf882338892f03bc37b906b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5b59d2eecee78c16f0",
+    "url": "/static/css/main.cb3a6e15.chunk.css"
+  },
+  {
+    "revision": "db34620c70cd806fc2c2",
+    "url": "/static/js/2.fad48857.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fad48857.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce5b59d2eecee78c16f0",
+    "url": "/static/js/main.fa5bb2d9.chunk.js"
+  },
+  {
+    "revision": "4b40cfa1f2cd2ffb143f",
+    "url": "/static/js/runtime-main.aab238ca.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
